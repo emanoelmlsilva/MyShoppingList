eb8de3e96b7489a01aa3326bf49c13bfa06715ed
emanoel <emanoel.silva83@dcx.ufpb.br>
2023-12-02 17:27:34 -0300

    ajusatdo codigo para melhorar o carregamento dos dados e usabiliadade

--- app/src/main/java/com/example/myshoppinglist/screen/HomeScreen.kt
+++ app/src/main/java/com/example/myshoppinglist/screen/HomeScreen.kt
@@ -6,17 +6,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.contentColorFor
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.myshoppinglist.callback.Callback
-import com.example.myshoppinglist.callback.VisibleCallback
 import com.example.myshoppinglist.components.BoxPurchaseHistoryComponent
 import com.example.myshoppinglist.components.CarouselComponent
 import com.example.myshoppinglist.components.HeaderComponent
+import com.example.myshoppinglist.database.dtos.UserDTO
 import com.example.myshoppinglist.fieldViewModel.HomeFieldViewModel
 import com.example.myshoppinglist.ui.theme.LatoBold
 import com.example.myshoppinglist.ui.theme.text_secondary
@@ -25,8 +27,15 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 @ExperimentalPagerApi
 @ExperimentalAnimationApi
 @Composable
-fun HomeScreen(navController: NavController?, homeFieldViewModel: HomeFieldViewModel) {
-    var visibleAnimation by remember { mutableStateOf(true) }
+fun HomeScreen(
+    navController: NavController?,
+    homeFieldViewModel: HomeFieldViewModel
+) {
+    val userDTO by homeFieldViewModel.getUser().collectAsState(UserDTO())
+    val purchaseCollection by homeFieldViewModel.getPurchaseCollection()
+        .collectAsState(initial = emptyList())
+    val creditCardCollection by homeFieldViewModel.getCreditCardCollection()
+        .collectAsState(initial = emptyList())
 
     Surface(
         color = MaterialTheme.colors.background,
@@ -37,25 +46,25 @@ fun HomeScreen(navController: NavController?, homeFieldViewModel: HomeFieldViewM
     ) {
 
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            HeaderComponent(
-                navController!!,
-                homeFieldViewModel.getIdAvatar(),
-                homeFieldViewModel.getNickName(),
-                visibleAnimation,
-                object : Callback {
-                    override fun onClick() {
-                        homeFieldViewModel.onChangeVisibleValue()
-                    }
-                })
+            if (userDTO != null) {
+                HeaderComponent(
+                    navController!!,
+                    userDTO!!.idAvatar,
+                    userDTO!!.nickName,
+                    true,
+                    object : Callback {
+                        override fun onClick() {
+                        }
+                    })
 
+            }
             CarouselComponent(
-                list = homeFieldViewModel.getCreditCardCollection(),
-                visibleAnimation = visibleAnimation,
+                list = creditCardCollection,
                 parentModifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight(.4f),
                 contentHeight = 265.dp,
-                navController = navController
+                navController = navController!!
             )
 
             Spacer(Modifier.size(32.dp))
@@ -72,14 +81,8 @@ fun HomeScreen(navController: NavController?, homeFieldViewModel: HomeFieldViewM
             Spacer(Modifier.size(24.dp))
 
             BoxPurchaseHistoryComponent(
-                homeFieldViewModel.getPurchaseCollection(),
-                object : VisibleCallback {
-                    override fun onChangeVisible(visible: Boolean) {
-                        if (visibleAnimation != visible) {
-                            visibleAnimation = visible
-                        }
-                    }
-                })
+                purchaseCollection
+            )
         }
     }
 
